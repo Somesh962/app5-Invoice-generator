3f82bbe27eb4721ad0fa8c8e754e8c4e8eff23b8
Somesh BR <somuresearch1@gmail.com>
2024-01-05 09:30:36 +0530

    Day26 : Add Table from Excel to Pdf

--- main.py
+++ main.py
@@ -23,7 +23,31 @@ for filepath in filepaths:
 
     # This is to display the date
     pdf.set_font(family="Times",size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Dated : {date}")
+    pdf.cell(w=50, h=8, txt=f"Dated : {date}",ln=2)
+
+    # Add Header for the table
+    columns = df.columns
+    # In order to remove the Underscores in the Header row of the table.
+    columns = [item.replace("_"," ").title() for item in columns]
+    # To set the color an font for the title
+    pdf.set_font(family="Times",size=12,style="B")
+    pdf.set_text_color(60,60,60)
+    pdf.cell(w=30,h=10,txt=columns[0],border=1)
+    pdf.cell(w=50,h=10,txt=columns[1],border=1)
+    pdf.cell(w=50,h=10,txt=columns[2],border=1)
+    pdf.cell(w=30,h=10,txt=columns[3],border=1)
+    pdf.cell(w=30,h=10,txt=columns[4],border=1,ln=1)
+
+    # Add rows
+    for index,row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(60, 60, 60)
+        pdf.cell(w=30,h=10,txt=str(row["product_id"]),border=1)
+        pdf.cell(w=50,h=10,txt=str(row["product_name"]),border=1)
+        pdf.cell(w=50,h=10,txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30,h=10,txt=str(row["price_per_unit"]),border=1)
+        pdf.cell(w=30,h=10,txt=str(row["total_price"]),border=1,ln=1)
+
 
 
     pdf.output(f"PDFs/{filename}.pdf")
